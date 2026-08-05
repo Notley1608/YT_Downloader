8511bd56d5b2f579ff6e4cc844bdb8f0b1c8b8d3
Notley1608 <oscarnotley@hotmail.co.uk>
2026-08-05 16:15:57 +1000

    oops

--- src/downloader.py
+++ src/downloader.py
@@ -1,6 +1,24 @@
+from pytubefix import YouTube
 import io
 import traceback
-from pytubefix import YouTube
+
+def get_video_info(url):
+    """
+    Given a YouTube URL, returns the video title and a list of available progressive streams.
+    Each stream is represented as a dict with keys: itag, resolution, filesize_mb
+    """
+    yt = YouTube(url)
+    streams = yt.streams.filter(progressive=True).order_by('resolution').desc()
+
+    stream_list = []
+    for stream in streams:
+        stream_list.append({
+            "itag": stream.itag,
+            "resolution": stream.resolution,
+            "filesize_mb": round(stream.filesize / (1024 * 1024), 2)
+        })
+
+    return yt.title, stream_list
 
 def download_video(url, itag):
     print(f"[DOWNLOAD] URL: {url}")
